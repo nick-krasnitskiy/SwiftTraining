589aa6af2334cfe27e11955732e95a93f71b14c3
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2023-10-09 15:42:28 +0300

    Add simple values.

--- ASwiftTour.playground/Contents.swift
+++ ASwiftTour.playground/Contents.swift
@@ -1,3 +1,55 @@
 import Cocoa
 
 var greeting = "Hello, playground"
+
+print("Hello, world!")
+// Prints "Hello, world!"
+
+// MARK: Simple Values
+
+var myVariable = 42
+myVariable = 50
+
+let myConstant = 42
+
+let implicitInteger = 70
+let implicitDouble = 70.0
+let explicitDouble: Double = 70
+
+let myConstant2: Float = 4
+
+let label = "The widht is "
+let width = 94
+let widhtLabel = label + String(width)
+
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+
+let quotation = """
+Even though there's whitespace to the left,
+the actual lines aren't indented.
+Except for this line.
+Double quotes (") can appear without being escaped.
+I still have \(apples + oranges) pieces of fruit.
+"""
+
+var fruits = ["strawberris", "limes", "tangerines"]
+fruits[1] = "grapes"
+
+var occupations = [
+    "Malcolm": "Captain",
+    "Kaylee": "Mechanic"
+]
+occupations["Jayne"] = "Public Relations"
+
+fruits.append("blueberries")
+print(fruits)
+// Prints "["strawberries", "grapes", "tangerines", "blueberries"]"
+
+fruits = []
+occupations = [:]
+
+let emptyArray: [String] = []
+let emptyDictionary: [String: Float] = [:]
